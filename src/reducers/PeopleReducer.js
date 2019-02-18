f87f404dfa5d8a3c26ccdd3507a71900b879a866
Gerard O'Hara <gerardo@fb.com>
2019-02-18 14:38:25 +0000

    fixed new screen having pre populated values

--- src/reducers/PeopleReducer.js
+++ src/reducers/PeopleReducer.js
@@ -44,14 +44,14 @@ export default (state = initialState, action) => {
         case 'NEW_CONTACT':
           return {
             ...state,
-            first_name: action.payload.first_name,
-            last_name: action.payload.last_name,
-            phone: action.payload.phone,
-            email: action.payload.email,
-            company: action.payload.company,
-            project: action.payload.project,
-            notes: action.payload.notes,
-            uid: action.payload.uid,
+            first_name: '',
+            last_name: '',
+            phone: '',
+            email: '',
+            company: '',
+            project: '',
+            notes: '',
+            uid: '',
           }
 
         case 'SAVE_CONTACT':
