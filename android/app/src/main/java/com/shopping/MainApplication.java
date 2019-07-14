7d067c3a3bc0b0479005475314fecf6ba9c90776
Your Name <you@example.com>
2019-07-14 19:47:14 +0100

    some android stuff and updated lodash package

--- android/app/src/main/java/com/shopping/MainApplication.java
+++ android/app/src/main/java/com/shopping/MainApplication.java
@@ -3,7 +3,6 @@ package com.shopping;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
-import com.reactnativecommunity.asyncstorage.AsyncStoragePackage;
 import com.swmansion.gesturehandler.react.RNGestureHandlerPackage;
 import com.oblador.vectoricons.VectorIconsPackage;
 import com.github.xinthink.rnmk.ReactMaterialKitPackage;
@@ -27,7 +26,6 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
-            new AsyncStoragePackage(),
             new RNGestureHandlerPackage(),
             new VectorIconsPackage(),
             new ReactMaterialKitPackage()
